5edaaa2b921265f53c9b238b52519d411e299e5b
kmilokun <109614875+kmilokun@users.noreply.github.com>
2022-07-23 16:37:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import operations as op
+
+def main():
+    print(op.suma(2, 1))
+    print(op.resta(5, 4))
+    print(op.multi(4, 1))
+    print(op.divi(6, 4))
+
+if __name__ == '__main__':
+    main()
